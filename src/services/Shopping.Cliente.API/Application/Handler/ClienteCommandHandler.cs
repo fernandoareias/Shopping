a106aa140359dfe14cd74b9f3cd8ec43d9106620
Fernando <nando.calheirosx@gmail.com>
2022-02-06 20:30:24 -0300

    Handler commands/events

---
+++ src/services/Shopping.Cliente.API/Application/Handler/ClienteCommandHandler.cs
@@ -0,0 +1,56 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using Shopping.Cliente.API.Application.Commands;
+using Shopping.Cliente.API.Application.Events;
+using Shopping.Cliente.API.Models;
+using Shopping.Cliente.API.Models.Interfaces;
+using Shopping.Cliente.API.Models.VOs;
+using Shopping.Cliente.API.Shared.Commands;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Shopping.Cliente.API.Application.Handler
+{
+    public class ClienteCommandHandler : CommandHandler,
+         IRequestHandler<RegistrarClienteCommand, ValidationResult>,
+         IRequestHandler<AdicionarEnderecoCommand, ValidationResult>
+    {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteCommandHandler(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.EhValido()) return message.ValidationResult;
+
+            var cliente = new Clientes(message.Id, message.Nome, message.Email, message.Cpf);
+
+            var clienteExistente = await _clienteRepository.ObterPorCpf(cliente.Cpf.Numero);
+
+            if (clienteExistente != null)
+            {
+                AdicionarErro("Este CPF já está em uso.");
+                return ValidationResult;
+            }
+
+            _clienteRepository.Adicionar(cliente);
+
+            cliente.AdicionarEvento(new ClienteRegistradoEvent(message.Id, message.Nome, message.Email, message.Cpf));
+
+            return await PersistirDados(_clienteRepository.UnitOfWork);
+        }
+
+        public async Task<ValidationResult> Handle(AdicionarEnderecoCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.EhValido()) return message.ValidationResult;
+
+            var endereco = new Endereco(message.Logradouro, message.Numero, message.Complemento, message.Bairro, message.Cep, message.Cidade, message.Estado, message.ClienteId);
+            _clienteRepository.AdicionarEndereco(endereco);
+
+            return await PersistirDados(_clienteRepository.UnitOfWork);
+        }
+    }
+}
