f2242f72c55ae564dd169ff2677564b4cfa9ddb8
Fernando <nando.calheirosx@gmail.com>
2022-02-06 18:16:40 -0300

    entity and VOs model

---
+++ src/services/Shopping.Cliente.API/Models/VOs/Cpf.cs
@@ -0,0 +1,14 @@
+﻿using Shopping.Cliente.API.Shared;
+
+namespace Shopping.Cliente.API.Models.VOs
+{
+    public class Cpf : Entity
+    {
+        public Cpf(string numero)
+        {
+            Numero = numero;
+        }
+
+        public string Numero { get; private set; }
+    }
+}
---
+++ src/services/Shopping.Cliente.API/Models/VOs/Email.cs
@@ -0,0 +1,18 @@
+﻿using Shopping.Cliente.API.Shared;
+
+namespace Shopping.Cliente.API.Models.VOs
+{
+    public class Email : Entity
+    {
+        // EF
+        protected Email() { }
+
+        public Email(string address)
+        {
+            Address = address;
+        }
+
+        public string Address { get; private set; }
+
+    }
+}
