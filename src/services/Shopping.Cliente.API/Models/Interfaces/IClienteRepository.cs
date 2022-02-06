6892d7f4260f474c7b30cfb6ca9789c2058ef0b5
Fernando <nando.calheirosx@gmail.com>
2022-02-06 18:16:51 -0300

    repository interface

---
+++ src/services/Shopping.Cliente.API/Models/Interfaces/IClienteRepository.cs
@@ -0,0 +1,18 @@
+﻿using Shopping.Cliente.API.Models.VOs;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shopping.Cliente.API.Models.Interfaces
+{
+    public interface IClienteRepository
+    {
+        void Adicionar(Cliente cliente);
+
+        Task<IEnumerable<Cliente>> ObterTodos();
+        Task<Cliente> ObterPorCpf(string cpf);
+
+        void AdicionarEndereco(Endereco endereco);
+        Task<Endereco> ObterEnderecoPorId(Guid id);
+    }
+}
