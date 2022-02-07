97e16e04426f40b728d49252bc5ffe5b00a228c5
Fernando <nando.calheirosx@gmail.com>
2022-02-07 20:43:19 -0300

    identity models

---
+++ src/services/Shopping.Identidade.API/Models/UsuarioToken.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Shopping.Identidade.API.Models
+{
+    public class UsuarioToken
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<UsuarioClaim> Claims { get; set; }
+    }
+}
