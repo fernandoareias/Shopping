f829ae037f71f6e860444ab35808c05eac888fd1
Fernando <nando.calheirosx@gmail.com>
2022-02-08 08:57:37 -0300

    notification implemented

---
+++ src/services/Shopping.Identidade.API/Shared/Notification/INotification.cs
@@ -0,0 +1,20 @@
+﻿
+
+using FluentValidation.Results;
+using System.Collections.Generic;
+
+namespace Shopping.Identidade.API.Shared.Notification
+{
+    public interface INotification
+    {
+        IEnumerable<string> Erros
+        {
+            get;
+        }
+
+        void AdicionarErro(string error);
+        void AdicionarErro(ValidationResult error);
+        void AdicionarErros(List<string> erros);
+        bool LimparErros();
+    }
+}
---
+++ src/services/Shopping.Identidade.API/Shared/Notification/Notification.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation.Results;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopping.Identidade.API.Shared.Notification
+{
+    public class Notification : INotification
+    {
+        internal List<string> _errors { get; private set; }
+
+        IEnumerable<string> INotification.Erros { get => _errors; }
+
+        public void AdicionarErro(string error)
+        {
+            _errors.Add(error);
+        }
+
+        public void AdicionarErros(List<string> erros)
+        {
+            erros.ForEach(e => _errors.Add(e));
+        }
+
+        public bool LimparErros()
+        {
+            _errors = new List<string>();
+            return !_errors.Any();
+        }
+
+        public void AdicionarErro(ValidationResult error)
+        {
+            error.Errors.ForEach(e => _errors.Add(e.ErrorMessage));
+        }
+
+        
+    }
+}
