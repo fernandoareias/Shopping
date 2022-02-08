7df621981f4bfb457437190b3f614a1777a48c4e
Fernando <nando.calheirosx@gmail.com>
2022-02-08 08:57:02 -0300

    interface -> identity service

---
+++ src/services/Shopping.Identidade.API/Models/Interfaces/IIdentityService.cs
@@ -0,0 +1,11 @@
+﻿using Shopping.Identidade.API.Shared.Responses;
+using System.Threading.Tasks;
+
+namespace Shopping.Identidade.API.Models.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response> Registro(UsuarioRegistro usuarioRegistro);
+        Task<Response> Login(UsuarioLogin usuarioLogin);
+    }
+}
