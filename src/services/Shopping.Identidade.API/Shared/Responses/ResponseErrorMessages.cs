01fedfcdf916d7e77e7c87151b7add3cbf43a380
Fernando <nando.calheirosx@gmail.com>
2022-02-08 08:57:52 -0300

    responses

---
+++ src/services/Shopping.Identidade.API/Shared/Responses/ResponseErrorMessages.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Shopping.Identidade.API.Shared.Responses
+{
+    public class ResponseErrorMessages : Response
+    {
+        public ResponseErrorMessages()
+        {
+            Mensagens = new List<string>();
+        }
+
+        public List<string> Mensagens { get; set; }
+    }
+}
