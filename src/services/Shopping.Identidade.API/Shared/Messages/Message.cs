558940aeee1b02a855fd0abf5503cf63d3df9241
Fernando <nando.calheirosx@gmail.com>
2022-02-09 18:59:00 -0300

    remove message bus

--- src/services/Shopping.Identidade.API/Shared/Messages/Message.cs
+++ src/services/Shopping.Identidade.API/Shared/Messages/Message.cs
@@ -5,11 +5,12 @@ namespace Shopping.Identidade.API.Shared.Messages
     public abstract class Message
     {
         public string MessageType { get; protected set; }
-        public Guid AggregateId { get; protected set; }
+        public Guid AggregateId { get;  set; }
 
         protected Message()
         {
             MessageType = GetType().Name;
+
         }
     }
 }
