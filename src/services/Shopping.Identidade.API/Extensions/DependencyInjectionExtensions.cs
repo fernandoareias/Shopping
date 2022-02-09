f4b2607707e68e33da37c57da85c974276c1c26c
Fernando <nando.calheirosx@gmail.com>
2022-02-09 19:01:40 -0300

    Extensions service

---
+++ src/services/Shopping.Identidade.API/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Shopping.Identidade.API.Shared.Messages.Bus;
+
+namespace Shopping.Identidade.API.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddMessageBus(this IServiceCollection services, string connection)
+        {
+            if (string.IsNullOrEmpty(connection)) throw new ArgumentNullException();
+
+            services.AddSingleton<IMessageBus>(new MessageBus(connection));
+
+            return services;
+        }
+    }
+}
