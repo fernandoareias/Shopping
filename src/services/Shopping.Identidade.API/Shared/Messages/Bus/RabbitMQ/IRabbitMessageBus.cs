e318c79998211e7f1c35ac7fce8dbf592fe5405b
Fernando <nando.calheirosx@gmail.com>
2022-02-11 20:50:43 -0300

    Base RabbitMQ Service Bus

---
+++ src/services/Shopping.Identidade.API/Shared/Messages/Bus/RabbitMQ/IRabbitMessageBus.cs
@@ -0,0 +1,13 @@
+﻿
+using Shopping.Identidade.API.Shared.Integration;
+using System;
+using System.Threading.Tasks;
+
+namespace Shopping.Identidade.API.Shared.Messages.Bus
+{
+    internal interface IRabbitMessageBus : IDisposable
+    {
+        Task<bool> Publisher(Event evento);
+
+    }
+}
---
+++ src/services/Shopping.Identidade.API/Shared/Messages/Bus/RabbitMQ/RabbitMessageBus.cs
@@ -0,0 +1,85 @@
+﻿
+using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Shopping.Identidade.API.Shared.Messages.Bus
+{
+    internal abstract class RabbitMessageBus : IRabbitMessageBus
+    {
+        private IConnection _connection;
+        private IModel _channel;
+
+        public virtual async Task<bool> Publisher(Event evento)
+        {
+            try
+            {
+                var message = JsonSerializer.Serialize(evento);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                _channel.BasicPublish(exchange: evento.MessageType,
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        private void TryConnect()
+        {
+            if (_connection.IsOpen) return;
+            
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            var policy = Policy.Handle<RabbitMQ.Client.Exceptions.ConnectFailureException>()
+                .Or<BrokerUnreachableException>()
+                .WaitAndRetry(3, retryAttempt =>
+                    TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+            policy.Execute(() =>
+            {
+                _connection = connectionFactory.CreateConnection();
+                _channel = _connection.CreateModel();
+                if (!_connection.IsOpen) OnDisconnect();
+            });
+        }
+
+        private void OnDisconnect()
+        {
+            var policy = Policy.Handle<RabbitMQ.Client.Exceptions.ConnectFailureException>()
+                .Or<BrokerUnreachableException>()
+                .RetryForever();
+
+            policy.Execute(TryConnect);
+        }
+
+        ~RabbitMessageBus()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+            _channel?.Dispose();
+        }
+
+    }
+}
+
