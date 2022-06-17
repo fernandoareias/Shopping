f0d154341c15e71ebc9aea6ac2fa31590678befd
fernandoareias <nando.calheirosx@gmail.com>
2022-06-17 09:51:39 -0300

    [Identidade] - Criação de config class

---
+++ src/services/Shopping.Identidade.API/Configuration/IdentityConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Identidade.API.Configuration
+{
+    public class IdentityConfig
+    {
+    }
+}
