f679e19feed00b8ff2519ad90e3b3e6c9a9a7e3c
fernandoareias <nando.calheirosx@gmail.com>
2022-06-18 13:19:35 -0300

    [Building Blocks] - Implementação dos Domain Object e base Repositories

---
+++ src/building blocks/Shopping.Core/DomainObjects/Data/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using Shopping.Core.DomainObjects.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping.Core.DomainObjects.Data.Interfaces
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+    }
+}
---
+++ src/building blocks/Shopping.Core/DomainObjects/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping.Core.DomainObjects.Interfaces
+{
+    public interface IAggregateRoot
+    {
+    }
+}
