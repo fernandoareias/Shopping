bba8e14db492487f4759b3c99ac11f32014ee7c9
fernandoareias <nando.calheirosx@gmail.com>
2022-06-18 13:20:57 -0300

    [Catalogo] - Implementação do repository da entidade produto

---
+++ src/services/Shopping.Catalogo.API/Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Catalogo.API.Models;
+using Shopping.Catalogo.API.Models.Interfaces;
+using Shopping.Core.DomainObjects.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Catalogo.API.Data.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly CatalogoContext _context;
+        private readonly IUnitOfWork _uow;
+
+        public ProdutoRepository(CatalogoContext context, IUnitOfWork uow)
+        {
+            _context = context;
+            _uow = uow;
+        }
+        public async Task<IEnumerable<Produto>> ObterTodosAsync()
+        {
+            return await _context.Produtos.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Produto> ObterPorId(Guid id)
+        {
+            return await _context.Produtos.FindAsync(id);
+        }
+
+        public void Adicionar(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            _uow.Commit();
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            _context.Produtos.Update(produto);
+            _uow.Commit();
+        }
+
+        public async void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
---
+++ src/services/Shopping.Catalogo.API/Models/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Shopping.Core.DomainObjects.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Catalogo.API.Models.Interfaces
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterTodosAsync();
+        Task<Produto> ObterPorId(Guid id);
+        void Adicionar(Produto produto);
+        void Atualizar(Produto produto);
+    }
+}
