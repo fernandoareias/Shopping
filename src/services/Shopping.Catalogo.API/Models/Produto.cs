691a6d23d5e4564beb81925651572ecb06c7d5ba
fernandoareias <nando.calheirosx@gmail.com>
2022-06-18 13:21:09 -0300

    [Catalogo] - Implementação da classe Produto

---
+++ src/services/Shopping.Catalogo.API/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using Shopping.Core.DomainObjects;
+using Shopping.Core.DomainObjects.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Catalogo.API.Models
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataInclusao { get; set; }
+        public string Imagem { get; set; }
+        public int QntEstoque { get; set; }
+    }
+}
