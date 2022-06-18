6ec094af4770ed588a86e5f354b80ff7462a40fd
fernandoareias <nando.calheirosx@gmail.com>
2022-06-18 13:21:57 -0300

    [Catalogo] - Map dos repositories e contexts

--- src/services/Shopping.Catalogo.API/Startup.cs
+++ src/services/Shopping.Catalogo.API/Startup.cs
@@ -2,10 +2,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Shopping.Catalogo.API.Data;
+using Shopping.Catalogo.API.Data.Repositories;
+using Shopping.Catalogo.API.Models.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +29,11 @@ namespace Shopping.Catalogo.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CatalogoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<CatalogoContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
