3e08be0dd3e873c01c9618a50615c535e91c976b
fernandoareias <nando.calheirosx@gmail.com>
2022-06-18 13:21:25 -0300

    [Catalogo] - Implementação do catalogo context e do UoW

---
+++ src/services/Shopping.Catalogo.API/Data/CatalogoContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Catalogo.API.Models;
+using Shopping.Core.DomainObjects.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Catalogo.API.Data
+{
+    public class CatalogoContext : DbContext, IUnitOfWork
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var prop in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                prop.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
