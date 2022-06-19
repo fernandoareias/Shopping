186e80056b241f264e6358eb19b2c75e0c1819b3
fernandoareias <nando.calheirosx@gmail.com>
2022-06-18 21:12:11 -0300

    [Building Blocks] - Inclusão de novos BBs

---
+++ src/building blocks/Shopping.Core/DomainObjects/Exceptions/DomainExceptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping.Core.DomainObjects.Exceptions
+{
+    public class DomainExceptions : Exception
+    {
+        public DomainExceptions()
+        {
+
+        }
+
+        public DomainExceptions(string message) : base(message)
+        {
+
+        }
+
+        public DomainExceptions(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
