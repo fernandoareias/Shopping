9ab8cddcb9e7ba2fda5de90b0ff7bafca9307ed0
fernandoareias <nando.calheirosx@gmail.com>
2022-06-18 21:16:51 -0300

    [Catalogo] - Inclusão da Authorization nos endpoints

--- src/services/Shopping.Catalogo.API/Controllers/CatalogoController.cs
+++ src/services/Shopping.Catalogo.API/Controllers/CatalogoController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Shopping.Catalogo.API.Models;
 using Shopping.Catalogo.API.Models.Interfaces;
+using Shopping.Core.WebAPI.Identidade;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace Shopping.Catalogo.API.Controllers
 {
+    [Authorize]
     [Route("api/catalogo")]
     [ApiController]
     public class CatalogoController : Controller
@@ -19,12 +22,14 @@ namespace Shopping.Catalogo.API.Controllers
             _produtoRepository = produtoRepository;
         }
 
+        [AllowAnonymous]
         [HttpGet("produtos")]
         public async Task<IEnumerable<Produto>> Index()
         {
             return await _produtoRepository.ObterTodosAsync();
         }
 
+        [ClaimsAuthorize("Catalogo", "ler")]
         [HttpGet("produtos/{id}")]
         public async Task<Produto> ProdutoDetalhe(Guid id)
         {
