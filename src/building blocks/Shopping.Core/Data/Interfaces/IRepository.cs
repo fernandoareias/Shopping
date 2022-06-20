9e6943a320b2be75aa5c6e824e0a7df6ff329ea7
fernandoareias <nando.calheirosx@gmail.com>
2022-06-19 21:10:38 -0300

    [Core] - Implementação do acesso a dados genérico

---
+++ src/building blocks/Shopping.Core/Data/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Shopping.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Core.Data.Interfaces
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+       IUnitOfWork UnitOfWork { get; }
+    }
+}
