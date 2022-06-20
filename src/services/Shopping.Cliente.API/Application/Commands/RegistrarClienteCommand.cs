28c618a70daede000e1d09b25aa8e846f1fc9cc8
fernandoareias <nando.calheirosx@gmail.com>
2022-06-19 21:17:22 -0300

    [Cliente] - Inclusão do command: Registrar cliente

---
+++ src/services/Shopping.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -0,0 +1,23 @@
+﻿using Shopping.Core.Messages;
+using System;
+
+namespace Shopping.Cliente.API.Application.Commands
+{
+    public class RegistrarClienteCommand : Command
+    {
+        public RegistrarClienteCommand(string nome, string email, string cpf)
+        {
+            
+            AggregateId = Guid.NewGuid();
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
+
+        
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+    }
+}
