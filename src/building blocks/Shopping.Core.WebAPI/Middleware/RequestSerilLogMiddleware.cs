f8f6deac4acf91f8c380d83a8fc5aafb85062259
fernandoareias <nando.calheirosx@gmail.com>
2022-06-19 21:15:18 -0300

    [Core.Service] - Inclusão de handler responsável por enviar os dados para o serilog

---
+++ src/building blocks/Shopping.Core.WebAPI/Middleware/RequestSerilLogMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Core.WebAPI.Middleware
+{
+    public class RequestSerilLogMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestSerilLogMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            using var property = LogContext.PushProperty("UserName", context?.User?.Identity?.Name ?? "anônimo");
+            return _next.Invoke(context);
+        }
+    }
+}
