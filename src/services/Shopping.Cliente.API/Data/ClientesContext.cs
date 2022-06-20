758d93d07987c2fb1c4c05eb3d582d77ebe58b94
fernandoareias <nando.calheirosx@gmail.com>
2022-06-19 21:18:55 -0300

    [Cliente] - Refatoração do startup e inclusão de novos packages

---
+++ src/services/Shopping.Cliente.API/Data/ClientesContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Core.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Cliente.API.Data
+{
+    public class ClientesContext : DbContext, IUnitOfWork
+    {
+
+        public ClientesContext(DbContextOptions<ClientesContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var prop in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                prop.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClientesContext).Assembly);
+        }
+
+        public DbSet<Shopping.Cliente.API.Models.Cliente> Cliente { get; set; }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
