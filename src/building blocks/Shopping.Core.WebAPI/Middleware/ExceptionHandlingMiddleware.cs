f99865fc2b86dd894634b66274a0f295b6882c34
fernandoareias <nando.calheirosx@gmail.com>
2022-06-19 21:15:00 -0300

    [Core.Service] - Inclusão de handler responsável por capturar as exceptions

---
+++ src/building blocks/Shopping.Core.WebAPI/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Core.WebAPI.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            Log.Error(exception, "Erro não tratado");
+
+            var code = HttpStatusCode.InternalServerError;
+
+            var result = System.Text.Json.JsonSerializer.Serialize(new { error = exception?.Message });
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
