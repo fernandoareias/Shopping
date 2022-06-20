6d039ff026672beadcaeb875634ba9a4079b8602
fernandoareias <nando.calheirosx@gmail.com>
2022-06-19 21:12:24 -0300

    [Core] - Implementação genérica do MediatR Handler

---
+++ src/building blocks/Shopping.Core/MediatR/MediatRHandler.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using Shopping.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Core.Mediator
+{
+    public class MediatRHandler : IMediatRHandler
+    {
+        private readonly IMediator _mediator;
+
+        public MediatRHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<ValidationResult> EnviarComando<T>(T comando) where T : Command
+        {
+            return await _mediator.Send(comando);
+        }
+
+        public async Task PublicarEvento<T>(T evento) where T : Event
+        {
+            await _mediator.Publish(evento);
+        }
+    }
+}
